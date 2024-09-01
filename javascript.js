d9ca6a0ff57ca2ad8cdcaac6d87c8c6b40540891
leecharlenej <leecharlenej@gmail.com>
2024-09-01 20:46:25 +0800

    Add draw and restart button.

--- javascript.js
+++ javascript.js
@@ -28,7 +28,7 @@ function Gameboard() {
     const dropToken = (row, column, player) => {
         // log(`dropToken running - ${player}`);
         const chosenCell = board[row][column].getValue();
-        if (chosenCell !== "_") {
+        if (chosenCell !== " ") {
             console.log(`ERROR: Cell has been chosen already - ${chosenCell}`)
             return;
         };
@@ -49,7 +49,7 @@ function Gameboard() {
 }
 
 function Cell() {
-    let value = "_";
+    let value = " ";
 
     const addToken = (player) => {
         value = player;
@@ -63,7 +63,7 @@ function GameController (
     playerOne = "X",
     playerTwo = "O"
 ) {
-    const board = Gameboard();
+    let board = Gameboard();
     const players = [
         {name: playerOne, token: "X"},
         {name: playerTwo, token: "O"}
@@ -102,53 +102,65 @@ function GameController (
         };
     }
 
-    printNewRound();
-    return {playRound, getActivePlayer, getBoard: board.getBoard};
-}
-
-function checkForWinner(board, token) {
-    log('checkForWinner running');
-    let boardCells = board.getBoard();
-
-    // Check rows
-    for (let i=0; i < 3; i++) {
-        let colOne = boardCells[i][0].getValue();
-        let colTwo = boardCells[i][1].getValue();
-        let colThree = boardCells[i][2].getValue();
-        // log(`[For row ${i}] colOne: ${colOne}, colTwo:${colTwo}, colThree: ${colThree}`);
-        
-        if((colOne === token) &&(colOne === colTwo && colTwo === colThree)) {
-            return colOne;
-        }
-    }
-
-    // Check columns
-    for (let j=0; j < 3; j++) {
-        let rowOne = boardCells[0][j].getValue();
-        let rowTwo = boardCells[1][j].getValue();
-        let rowThree = boardCells[2][j].getValue();
-        log(`[For col ${j}] rowOne: ${rowOne}, rowTwo: ${rowTwo}, rowThree: ${rowThree}`);
-        log(rowOne === rowTwo === rowThree);
+    const checkForWinner = (board, token) => {
+        log('checkForWinner running');
+        let boardCells = board.getBoard();
+    
+        // Check rows
+        for (let i=0; i < 3; i++) {
+            let colOne = boardCells[i][0].getValue();
+            let colTwo = boardCells[i][1].getValue();
+            let colThree = boardCells[i][2].getValue();
+            // log(`[For row ${i}] colOne: ${colOne}, colTwo:${colTwo}, colThree: ${colThree}`);
             
-        if((rowOne === token) && (rowOne === rowTwo && rowTwo === rowThree)) {
-            return rowOne;
+            if((colOne === token) &&(colOne === colTwo && colTwo === colThree)) {
+                return colOne;
+            }
         }
-    }
+    
+        // Check columns
+        for (let j=0; j < 3; j++) {
+            let rowOne = boardCells[0][j].getValue();
+            let rowTwo = boardCells[1][j].getValue();
+            let rowThree = boardCells[2][j].getValue();
+            log(`[For col ${j}] rowOne: ${rowOne}, rowTwo: ${rowTwo}, rowThree: ${rowThree}`);
+            log(rowOne === rowTwo === rowThree);
+                
+            if((rowOne === token) && (rowOne === rowTwo && rowTwo === rowThree)) {
+                return rowOne;
+            }
+        }
+    
+        // Check diagonals
+        let leftTop = boardCells[0][0].getValue();
+        let middle = boardCells[1][1].getValue();
+        let leftBottom = boardCells[2][2].getValue();
+    
+        let rightTop = boardCells[0][2].getValue();
+        let rightBottom = boardCells[2][0].getValue();
+    
+        if ((middle === token) &&((leftTop === middle && middle === leftBottom) || (rightTop === middle && middle === rightBottom))){
+            return middle;
+        }
+    
+        return false;
+    };
 
-    // Check diagonals
-    let leftTop = boardCells[0][0].getValue();
-    let middle = boardCells[1][1].getValue();
-    let leftBottom = boardCells[2][2].getValue();
+    const restartBoard = () => {
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++) {
+                board.getBoard()[i][j] = Cell(); // Reset each cell to an empty Cell
+            }
+        }
+        activePlayer = players[0];
+        printNewRound();
+    };
 
-    let rightTop = boardCells[0][2].getValue();
-    let rightBottom = boardCells[2][0].getValue();
-
-    if ((middle === token) &&((leftTop === middle && middle === leftBottom) || (rightTop === middle && middle === rightBottom))){
-        return middle;
-    }
-
-    return false;
+    printNewRound();
+    return {playRound, getActivePlayer, getBoard: board.getBoard, restartBoard};
 }
+
+
     
 // This is for console version.
 // const game = GameController();
@@ -157,7 +169,10 @@ function ScreenController() {
     const game = GameController();
     const playerTurnDiv = document.querySelector('.turn');
     const boardDiv = document.querySelector('.board');
+    const restartButton = document.querySelector('#restart-btn');
     let winnerFlag = null;
+    let drawCount = 0;
+
   
     const updateScreen = () => {
       // clear the board
@@ -170,6 +185,8 @@ function ScreenController() {
       // Display player's turn
       if (winnerFlag) {
         playerTurnDiv.textContent = `Player ${winnerFlag} wins!`;
+    } else if (drawCount===9){
+        playerTurnDiv.textContent = `It's a draw!`;
     } else {
         // Display player's turn
         playerTurnDiv.textContent = `Player ${activePlayer.name}'s turn...`;
@@ -192,11 +209,20 @@ function ScreenController() {
         })
       })
     }
+
+    const restartBoard = () => {
+        winnerFlag = null;
+        drawCount = 0;
+        game.restartBoard();
+        updateScreen();
+        log(`Board restarted.`)
+    }
+    restartButton.addEventListener("click", restartBoard);
   
     // Add event listener for the board
     function clickHandlerBoard(e) {
         
-        if (winnerFlag){return;}
+        if (winnerFlag || drawCount === 9){return;}
 
         const selectedRow = e.target.dataset.row;
         const selectedColumn = e.target.dataset.column;
@@ -204,17 +230,22 @@ function ScreenController() {
         if (!selectedRow && !selectedColumn) return;
       
         winnerFlag = game.playRound(selectedRow, selectedColumn);
+        drawCount++;
         log(`clickHandlerBoard: ${winnerFlag ==='X'}`);
         
         updateScreen();
     }
-    
+
     boardDiv.addEventListener("click", clickHandlerBoard);
+
+
   
     // Initial render
     updateScreen();
   
     // We don't need to return anything from this module because everything is encapsulated inside this screen controller.
-  }
+}
   
-  ScreenController();
+ScreenController();
+
+
